e814cd36ff2e40121f136bd3676b490f7fae5b97
Cazgem <cazgem@gmail.com>
2021-07-26 03:18:11 +0000

    File Cleanup

--- mongo.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-module.exports = async (mongopath) => {
-    await mongoose.connect(`${mongopath}`, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
-    return mongoose
-}
